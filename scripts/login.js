e758e3ac770747476af78ccd30b6531a314dc1af
Abdul Salam <abdulsalampk550@gmail.com>
2025-11-09 22:31:54 +0500

    Login page added

---
+++ scripts/login.js
@@ -0,0 +1,42 @@
+import { signInWithEmailAndPassword, signInWithPopup ,GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { auth, provider } from "./firebaseconfig.js";
+
+const form = document.querySelector("#form");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+const googleBtn = document.querySelector("#googleSignIn");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user)
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+        email.value = "";
+        password.value = "";
+});
+
+googleBtn.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log(token, user)
+        }).catch((error) => {
+            
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            
+        });
+})
