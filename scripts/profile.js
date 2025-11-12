d0cd545022e77835b01dc7b2f7898db2faa431d6
Abdul Salam <abdulsalampk550@gmail.com>
2025-11-13 01:14:38 +0500

    profile page completed

--- scripts/profile.js
+++ scripts/profile.js
@@ -7,14 +7,35 @@ const profile = document.querySelector(".profile-avatar");
 const email = document.querySelector("#email");
 const userId = document.querySelector("#uid");
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    
-    const uid = user.uid;
-    
-  } else {
-    // User is signed out
-    // ...
-  }
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+
+        const uid = user.uid;
+        console.log(uid)
+        const userInfo = await getData(uid, "users");
+        console.log(userInfo);
+        profile.src = userInfo[0].profile;
+        fullName.textContent = userInfo[0].fullName;
+        email.textContent = userInfo[0].email
+        userId.textContent = userInfo[0].uid
+
+
+
+    } else {
+        window.location = ""
+    }
 });
+
+async function getData(uid, collections) {
+    let data = []
+    const q = query(collection(db, collections),
+        where("uid", "==", uid));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        // console.log(`${doc.id} => ${doc.data()}`);
+        data.push(doc.data());
+    });
+    return data
+
+}
 
