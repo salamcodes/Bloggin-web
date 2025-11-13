9356ae8b797bb2f315ad6e381d224c1fd42e94a4
Abdul Salam <abdulsalampk550@gmail.com>
2025-11-13 23:59:50 +0500

    features added

---
+++ scripts/user.js
@@ -0,0 +1,37 @@
+import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js"
+
+import { db } from "./firebaseconfig.js"
+
+const profileName = document.querySelector("#name");
+const profile = document.querySelector("#userProfile");
+const email = document.querySelector("#email");
+const container = document.querySelector(".posts-container");
+
+let posts = []
+let userId = localStorage.getItem("userId")
+let userProfile;
+
+async function getData(uid, collections) {
+    let data = []
+    const q = query(collection(db, collections),
+        where("uid", "==", uid));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        data.push(doc.data())
+    });
+    return data;
+
+}
+async function arrivedData() {
+    const userInfo = await getData(userId, "users");
+    // console.log(userInfo);
+    userProfile = userInfo[0].profile;
+    profile.src = userProfile;
+    profileName.textContent = userInfo[0].fullName;
+    email.textContent = userInfo[0].email;
+    const postInfo = await getData(userId, "posts");
+
+    posts.push(...data);
+}
+arrivedData()
